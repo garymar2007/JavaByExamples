429cd5f9e3704085ea6a7e1ab3f2eeec4bfa1c59
Gary Ma <gary.ma@csgi.com>
2023-04-20 17:43:06 +0200

    Added array examples

---
+++ src/problemSolving/SubarrayDivision.java
@@ -0,0 +1,41 @@
+package problemSolving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class SubarrayDivision {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = birthday(s, d, m);
+        System.out.println(result);
+        bufferedReader.close();
+    }
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        int counter = 0;
+        for(int i = 0; i + m <= s.size(); i++){
+            int sum = s.get(i);
+            for(int j = 1; j < m; j++) {
+                sum += s.get(i + j);
+            }
+            if(sum % d == 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
